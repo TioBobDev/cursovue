f6962f1ac1412d551ed0327aeaa57295e2f64512
TioBobDev <bobdev@outlook.com.br>
2025-02-17 16:12:31 -0400

    Curso de Vue 3: #07 - Entendendo os Lifecycle hooks (ciclo de vida)

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
     <div>
       <PrimeiroComponente />
       <p>{{ nome }}</p>
+      <LifeCycle />
     </div>
 
 </template>
 
 <script>
+import LifeCycleVue from './components/LifeCycle.vue'
 
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
   
 export default {
     name: 'App',
     components: {
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle,
     }, 
     data() {
       return {
