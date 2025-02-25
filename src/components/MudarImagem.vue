8a5e34e3d4c0bb86e66e9612910533ba24696ec1
TioBobDev <bobdev@outlook.com.br>
2025-02-25 08:57:14 -0400

    Curso de Vue 3: #18 - Emit

---
+++ src/components/MudarImagem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <button @click="$emit('mudarImagem')">TROCAR IMAGEM DE PERFIL</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'MudarImagem',
+        emits: ['mudarImagem']
+    }
+</script>
--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -2,13 +2,18 @@
     <button @click="mostrarFoto">Mostrar Foto</button>
     <span v-show="verFoto">Foto sendo mostrada</span>
     <img v-show="verFoto" class="size-picture" :src="profile" :alt="descricao">
-    
+    <MudarImagem @mudar-imagem="trocarImagem"/> <!-- USA O EMIT PARA ENVIAR UM EVENTO PARA O COMPONENTE PAI -->
 </template>
 
 <script>
 
+import MudarImagem from './MudarImagem.vue'
+
 export default {
     name: 'Picture',
+    components:{
+        MudarImagem
+    },
     data() {
         return {
             profile: "/img/profile.png",
@@ -19,6 +24,9 @@ export default {
     methods: {
         mostrarFoto(){
             this.verFoto =!this.verFoto;
+        },
+        trocarImagem(){
+            this.profile = "/img/profile2.png"
         }
     }
 }
